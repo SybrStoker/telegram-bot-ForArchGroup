0a403a0bbcab6e60c7d4b78c4f7a90ede75e86d3
SybrStoker <gorindan2@gmail.com>
2022-02-16 13:00:15 +0700

    Added function to output text properly and we have the repo of replies

--- bot.py
+++ bot.py
@@ -1,56 +1,20 @@
+import pathlib
+from pathlib import Path
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import TOKEN
 
-#i will change it soon for now it's like test.
-#Use file in next update
-text = """
-Пакеты первой необходимости:
+def output_reply(file_name:str):
+    """Find the needed replies
 
-Пример:
-Номер."Название пакета" - (описание);
-
-/*При перечислении пакетов, нужно что-то выбрать или использовать все вместе при желании, допустим чтобы попробовать все и решить, что нравиться больше.*/
-
-
-1."base" - ();
-
-2. "linux" - ();
-
-3.linux-firmware - (прошивка);
-
-4.base-devel - ();
-
-5.networkmanager - (программа для автоматического подключения интернета);
-
-6.grub - (загрузчик системы);
-
-7.thunar - (проводник или же файловый менеджер. Графическая версия для перехода по каталогам);
-
-8.xorg - (Демон для запуска графической оболочки);
-
-9."plasma", "gnome", "lxqt" - (Графическая Оболочка);
-
-10.sddm - (графический менеджер, позволяет выбирать ГО и пользователя);
-
-11. "nvidia" - (nvidia драйвер для большей части видюх);
-
-12. "vim" - (консольный текстовый редактор);
-
-13. "net-tools" - (набор команд для работы с интернетом, сюда же входит ipconfig);
-
-14. "wget" - (консольная ультилита для загрузки файлов через URL);
-
-15. "git" - (консольная ультилита для поддержки версий программ);
-
-16. "firefox" - (браузер);
-
-17."alacritty" - (терминал);
-
-18. "htop" - (терминальная ультилита которая позволяеет увидеть загруженность железа и запущенные процессы с демонами);
-"""
+    We take the name of reply and seek it in rep, when we found it,
+    give the inner text as output"""
+    with open(Path("replies", file_name)) as file:
+        text = file.read()
+    return text
 
 #init bot
 bot = Bot(token=TOKEN)
@@ -67,7 +31,11 @@ async def process_help_command(message: types.Message):
 
 @dp.message_handler(commands=['packages'])
 async def process_show_source_command(message: types.Message):
-    await message.reply(text)
+    await message.reply(output_reply("package_reply"))
+
+@dp.message_handler(commands=['addingUser'])
+async def process_show_source_command(message: types.Message):
+    await message.reply(output_reply("add_user_reply"))
 
 #to check out is bot working
 @dp.message_handler()
---
+++ config.py
@@ -0,0 +1,2 @@
+#token of my test telegram bot
+TOKEN = "5276348835:AAGPmgykYacEaKcyD-UUd9Djr6BK3nNkI_Q"
